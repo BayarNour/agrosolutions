78befdc5d7befc6847f1389c5653d3e9b5efc97a
Nour Bayar <nourbayar4@gmail.com>
2024-11-04 23:58:58 +0100

    agro

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Agrosolution';
+  myimage:string="assets/logo/logo.png";
+  im:string="assets/logo/phoo.jpg"
+}
+
